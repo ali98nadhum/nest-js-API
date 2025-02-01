df9af99d630cfdbd937ef9a478ce5b842a1702a2
Ali nadhum <alinadhum1998@gmail.com>
2025-02-01 08:37:37 +0300

    edit login dto

--- src/users/dtos/login.dto.ts
+++ src/users/dtos/login.dto.ts
@@ -1,14 +1,19 @@
-import {IsEmail,IsNotEmpty,IsString,MaxLength,MinLength} from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class LoginDto {
-    @IsEmail()
-      @MaxLength(250)
-      @IsNotEmpty()
-      email: string;
-    
-      @IsString()
-      @MinLength(8)
-      @IsNotEmpty()
-      password: string;
-    
+  @IsEmail()
+  @MaxLength(250)
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  @IsNotEmpty()
+  password: string;
 }
