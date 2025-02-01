7a2d2d76662874c40272b5d3a2a311be6a255fed
Ali nadhum <alinadhum1998@gmail.com>
2025-02-01 08:36:44 +0300

    edit app revew

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,13 +1,12 @@
-import { Module } from "@nestjs/common";
-import { ReviewsController } from "./reviews.controller";
-import { ReviewsService } from "./reviews.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Review } from "./review.entity";
-
+import { Module } from '@nestjs/common';
+import { ReviewsController } from './reviews.controller';
+import { ReviewsService } from './reviews.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Review } from './review.entity';
 
 @Module({
-    controllers: [ReviewsController],
-    providers: [ReviewsService],
-    imports: [TypeOrmModule.forFeature([Review])]
+  controllers: [ReviewsController],
+  providers: [ReviewsService],
+  imports: [TypeOrmModule.forFeature([Review])],
 })
-export class ReviewsModule{}
+export class ReviewsModule {}
