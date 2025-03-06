1aae24f581a3d2594b72f612a9df8bb3032953e0
Ali nadhum <alinadhum1998@gmail.com>
2025-03-06 14:47:19 +0300

    add createNewReview controller

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,14 +1,28 @@
-import { Controller , Get } from "@nestjs/common";
+import { Body, Controller , Get, Param, ParseIntPipe, Post, UseGuards } from "@nestjs/common";
 import { ReviewsService } from "./reviews.service";
+import { CreateReviewDto } from "./dtos/create-review.dto";
+import { CurrentUser } from "src/users/decorators/current-user.decorator";
+import { JwtPayloadType } from "src/utils/types";
+import { AuthRolesGuard } from "src/users/guards/auth.roles.guard";
+import { Roles } from "src/users/decorators/user.role.decorator";
+import { UserType } from "src/utils/enums";
 
 
-@Controller({})
+@Controller("api/reviews")
 export class ReviewsController{
 
 
     constructor(private readonly reviewService: ReviewsService){}
-    @Get("api/reviews")
-    public getAllReviews(){
-        return this.reviewService.getAll();
+    
+
+
+    @Post(':productId')
+    @UseGuards(AuthRolesGuard)
+    @Roles(UserType.ADMIN , UserType.NORMAL_USER)
+    public createNewReview(@Param('productId' , ParseIntPipe) productId:number,@Body() body: CreateReviewDto,@CurrentUser() payload: JwtPayloadType){
+
+        return this.reviewService.createReview(productId , payload.id , body)
     }
-}
+
+
+    }
