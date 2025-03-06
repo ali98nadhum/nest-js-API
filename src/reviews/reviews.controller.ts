49999fef3f39bb1b32514ab27816a60ba33eaaab
Ali nadhum <alinadhum1998@gmail.com>
2025-03-06 15:08:31 +0300

    edit createNewReview fun

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -8,21 +8,18 @@ import { Roles } from "src/users/decorators/user.role.decorator";
 import { UserType } from "src/utils/enums";
 
 
-@Controller("api/reviews")
-export class ReviewsController{
+@Controller('api/reviews')
+export class ReviewsController {
+  constructor(private readonly reviewService: ReviewsService) {}
 
-
-    constructor(private readonly reviewService: ReviewsService){}
-    
-
-
-    @Post(':productId')
-    @UseGuards(AuthRolesGuard)
-    @Roles(UserType.ADMIN , UserType.NORMAL_USER)
-    public createNewReview(@Param('productId' , ParseIntPipe) productId:number,@Body() body: CreateReviewDto,@CurrentUser() payload: JwtPayloadType){
-
-        return this.reviewService.createReview(productId , payload.id , body)
-    }
-
-
-    }
+  @Post(':productId')
+  @UseGuards(AuthRolesGuard)
+  @Roles(UserType.ADMIN, UserType.NORMAL_USER)
+  public createNewReview(
+    @Param('productId', ParseIntPipe) productId: number,
+    @Body() body: CreateReviewDto,
+    @CurrentUser() payload: JwtPayloadType,
+  ) {
+    return this.reviewService.createReview(productId, payload.id, body);
+  }
+}
