db7f8dcc88504b669dbbd3656d87af05046cf142
Ali nadhum <alinadhum1998@gmail.com>
2025-03-09 14:25:21 +0300

    Cofing mail module

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,5 +1,26 @@
 import { Module } from "@nestjs/common";
+import { MailerModule } from "@nestjs-modules/mailer";
+import { ConfigService } from "@nestjs/config";
 
 
-@Module({})
+@Module({
+    imports: [
+        MailerModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => {
+                return {
+                    transport: {
+                        host: config.get<string>("SMTP_HOST"),
+                        port: config.get<number>("SMTP_PORT"),
+                        secure: false,
+                        auth: {
+                            user: config.get<string>("SMTP_USERNAME"),
+                            pass: config.get<string>("SMTP_PASSWORD")
+                        }
+                    }
+                }
+            }
+        })
+    ]
+})
 export class MailModule{}
